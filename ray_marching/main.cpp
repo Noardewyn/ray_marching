a686b1f060e3f42610d12a2cce290bfedce3b7e2
Noardewyn <karpovichalexej@gmail.com>
2020-03-18 16:23:27 +0300

    implementation of working, but not effective scene render algorithm

--- ray_marching/main.cpp
+++ ray_marching/main.cpp
@@ -1,59 +1,105 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
 
-void drawScene(sf::RenderWindow &window)
-{
-
+float VectorLength(const Vector3f& vec) {
+  return std::powf(vec.x, 2) + std::powf(vec.y, 2) + std::powf(vec.z, 2);
 }
 
-int main()
+Vector3f Normalized(const Vector3f& vec) {
+  float vecLength = VectorLength(vec);
+  return Vector3f(vec.x / vecLength, vec.y / vecLength, vec.z / vecLength);
+}
+
+void drawScene(sf::RenderWindow& window, VertexArray& view) {
+  static Vector3f camera(0.0, 1.0, 0.0);
+  static Vector3f rayDir(Normalized(Vector3f()));
+
+  for (uint64_t y = 0; y < window.getSize().y; y++) {
+    for (uint64_t x = 0; x < window.getSize().x; x++) {
+      const float nx = static_cast<float>(x) / window.getSize().x;
+      const float ny = static_cast<float>(y) / window.getSize().y;
+      const auto currentArrayIndex = y * window.getSize().x + x;
+
+      view[currentArrayIndex].color = Color(0, ny * 250, 0);
+    }
+  }
+
+  window.draw(view);
+}
+
+void ResizeView(VertexArray& view, uint64_t width, uint64_t height)
 {
-	sf::Font font;
+  const auto pixelCount = width * height;
+  view.clear();
+  view.resize(pixelCount);
 
-    if (!font.loadFromFile("Roboto-Black.ttf"))
-	{
-		std::cerr << "ERROR: font not found" << std::endl;
-	}
+  for (uint64_t y = 0; y < height; y++) {
+    for (uint64_t x = 0; x < width; x++) {
+      const auto currentArrayIndex = y * width + x;
+      view[currentArrayIndex].position = Vector2f(x, height - y);
+    }
+  }
+}
 
-	RenderWindow window(VideoMode(800, 600), "ray_marching");
+int main() {
+  sf::Font font;
 
-	window.setFramerateLimit(60);
+  if (!font.loadFromFile("Roboto-Black.ttf")) {
+    std::cerr << "ERROR: font not found" << std::endl;
+  }
 
-	sf::Clock clock;
-	sf::Time time;
+  RenderWindow window(VideoMode(300, 400), "ray_marching");
+  window.setFramerateLimit(60);
 
-	float fps = 0;
+  sf::Clock clock;
+  sf::Time time;
 
-	sf::Text fpsLabel;
-	fpsLabel.setFont(font);
-	fpsLabel.setCharacterSize(26);
-	fpsLabel.setFillColor(sf::Color::White);
-	fpsLabel.setStyle(sf::Text::Bold);
+  float fps = 0;
 
-	while (window.isOpen())
-	{
-		time = clock.getElapsedTime();
-		clock.restart().asSeconds();
-		fps = 1.0f / time.asSeconds();
+  sf::Text fpsLabel;
+  fpsLabel.setFont(font);
+  fpsLabel.setCharacterSize(26);
+  fpsLabel.setFillColor(sf::Color::White);
+  fpsLabel.setStyle(sf::Text::Bold);
 
-		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-				window.close();
-		}
+  static sf::VertexArray view(sf::Points);
+  ResizeView(view, window.getSize().x, window.getSize().y);
 
-		window.clear(Color(0, 0, 0, 0));
+  while (window.isOpen()) {
+    time = clock.getElapsedTime();
+    clock.restart().asSeconds();
+    fps = 1.0f / time.asSeconds();
 
-		drawScene(window);
+    Event event;
+    while (window.pollEvent(event)) {
+      switch (event.type) {
+        case sf::Event::Closed: {
+          window.close();
+          break;
+        }
 
-		fpsLabel.setString(std::to_string(static_cast<int>(fps)));
-	    window.draw(fpsLabel);
+        case sf::Event::Resized: {
+          window.setView(View(FloatRect(0, 0, event.size.width, event.size.height)));
+          ResizeView(view, event.size.width, event.size.height);
+          break;
+        }
 
-		window.display();
-	}
+        default:
+          break;
+      }
+    }
 
-	return 0;
+    window.clear(Color(0, 0, 0));
+
+    drawScene(window, view);
+
+    fpsLabel.setString(std::to_string(static_cast<int>(fps)));
+    window.draw(fpsLabel);
+
+    window.display();
+  }
+
+  return 0;
 }
